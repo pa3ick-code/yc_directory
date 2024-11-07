1c59224d4f30aa14a9220ee965ce823c1086a68d
patthrills <asubiaropatrick@gmail.com>
2024-11-07 09:42:00 +0100

    Installed shadcn UI, added hero section, and search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {searchParams: Promise<{ query?: string}>}) {
+  const query = (await searchParams).query
   return (
-    <div>Home</div>
+    <>
+      <section className="pink_container">
+        <div className="heading">Pitch your startup, <br /> connect with entrepreneurs</div>
+        <p className="sub-heading !max-w-2xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+
+        <SearchForm query={query}/>
+      </section>
+    </>
   );
 }
